79517f6e8360304d3084f98833277c91fa61f2cc
ishratslh <ishratsalehuddin@gmail.com>
2023-07-26 15:42:19 +0200

    Chore: add genSignature and parameters of run single verification (l.109 to do another way)

--- nodes/IcypeasSingle/IcypeasSingle.node.ts
+++ nodes/IcypeasSingle/IcypeasSingle.node.ts
@@ -53,6 +53,7 @@ export class IcypeasSingle implements INodeType {
 				placeholder: 'Email to search',
 				description: 'Email to search',
 			},
+			
 		],
 	};
 
@@ -64,34 +65,82 @@ export class IcypeasSingle implements INodeType {
 		const items = this.getInputData();
 
 		let item: INodeExecutionData;
-		let myString: string;
+		const apiKey = this.getNodeParameter('apiKey', 0) as string;
+		const apiSecret = this.getNodeParameter('apiSecret', 0) as string;
+		const userId = this.getNodeParameter('userId', 0) as string;
 
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+		const URL = "https://app.icypeas.com/api/email-verification";
+		const METHOD = "POST";
+
+		// Function to generate the signature
+		const genSignature = (
+			url: string,
+			method: string,
+			secret: string,
+			timestamp: string = new Date().toISOString()
+		) : string => {
+			const URL = require('url').URL;
+			const Crypto = require('crypto');
+			const endpoint = new URL(url).pathname;
+			const payload = `${method}${endpoint}${timestamp}`.toLowerCase();
+			const sign = Crypto.createHmac("sha1", secret).update(payload).digest("hex");
+	
+			return sign;
+		};
+
+		// Iterates over all input items and add the key "response" with the result of the API call
+		for (const item of items) {
 			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
-				item = items[itemIndex];
+				const email = item.json.email as string; // Get the email value from the input item
 
-				item.json['myString'] = myString;
-			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
-				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+				const timestamp = new Date().toISOString();
+				const signature = genSignature(URL, METHOD, apiSecret, timestamp);
+
+				const bodyParameters = { email, };
+
+				const headersParameters = { "X-ROCK-TIMESTAMP": timestamp,};
+
+				// Make the API call with the provided parameters (apiKey, apiSecret, userId, etc.)
+				const response = await this.helpers.request({
+					method: 'POST',
+					url: URL,
+					body: bodyParameters,
+					headers: {
+						Authorization: `Basic ${Buffer.from(`${apiKey}:${apiSecret}`).toString('base64')}`,
+					},
+				});
+
+				if (response.success) {
+					// If the API call was successful
+					item.json['response'] = {
+						success: true,
+						message: 'Email verification successful!',
+						data: {
+							email,
+							status: response.item?.status,
+							// Add other relevant data here based on the Icypeas API response
+						},
+					};
 				} else {
-					// Adding `itemIndex` allows other workflows to handle this error
-					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
-						error.context.itemIndex = itemIndex;
-						throw error;
+					// If the API call was not successful, check for specific errors
+					if (response.validationErrors) {
+						// If there are validation errors
+						const errorMessage = response.validationErrors.map((error: any) => error.message).join(', ');
+						throw new NodeOperationError(this.getNode(), errorMessage);
+					} else if (response.error === 'UnauthorizedAccessError') {
+						// If there was an unauthorized access error
+						throw new NodeOperationError(this.getNode(), 'Unauthorized access. Please check your API credentials.');
+					} else {
+						// If there was a generic error
+						throw new NodeOperationError(this.getNode(), 'An unknown error occurred while processing the request.');
 					}
-					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
-					});
 				}
+			} catch (error) {
+				// If there's an error, set the response message to indicate an error occurred
+				item.json['response'] = {
+					success: false,
+					message: 'An error occurred while processing the item.',
+				};
 			}
 		}
 
