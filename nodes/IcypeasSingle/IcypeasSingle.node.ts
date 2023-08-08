870857857f560c6e8761b15f29e7fde2852af158
ishratslh <ishratsalehuddin@gmail.com>
2023-08-08 12:02:55 +0200

    Chore! : implementation of bulk in single

--- nodes/IcypeasSingle/IcypeasSingle.node.ts
+++ nodes/IcypeasSingle/IcypeasSingle.node.ts
@@ -238,8 +238,7 @@ export class IcypeasSingle implements INodeType {
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const credentials = await this.getCredentials('icypeasApi');
-		//const searchType = this.getNodeParameter('searchType', 0);
-		//if ( searchType === 'singleSearch') {}
+		const searchType = this.getNodeParameter('searchType', 0);
 
 		if ( !credentials.apiKey || !credentials.apiSecret) {
 			throw new NodeOperationError(this.getNode(), 'Credentials are missing.');
@@ -247,65 +246,230 @@ export class IcypeasSingle implements INodeType {
 		const apiKey = credentials.apiKey as string;
       	const apiSecret = credentials.apiSecret as string;
 
-		const URL_email_verif = "https://app.icypeas.com/api/email-verification";
-		const URL_email_search = "https://app.icypeas.com/api/email-search";
-		const URL_domain_search = "https://app.icypeas.com/api/domain-search";
-
 		try {
-			const taskSingle = this.getNodeParameter('taskSingle', 0);
+			if ( searchType === 'singleSearch') {
 
-			if ( taskSingle === 'email-verification') {
+				const URL_email_verif = "https://app.icypeas.com/api/email-verification";
+				const URL_email_search = "https://app.icypeas.com/api/email-search";
+				const URL_domain_search = "https://app.icypeas.com/api/domain-search";
+
+				const taskSingle = this.getNodeParameter('taskSingle', 0);
+
+				if ( taskSingle === 'email-verification') {
+					const timestamp = new Date().toISOString();
+					const signature = generateSignature(URL_email_verif, "POST", apiSecret, timestamp);
+					const headers = {
+						"Content-Type": "application/json",
+						Authorization: `${apiKey}:${signature}`,
+						"X-ROCK-TIMESTAMP": timestamp,
+					};
+
+					const email = this.getNodeParameter('email', 0) as string; // Get the email value from the node properties
+					const bodyParameters = JSON.stringify({ email });
+
+					const outputData = await processApiCallSingle(URL_email_verif, headers, bodyParameters);
+        			return [outputData];
+
+				}else if ( taskSingle === 'email-search') {
+					const timestamp = new Date().toISOString();
+					const signature = generateSignature(URL_email_search, "POST", apiSecret, timestamp);
+					const headers = {
+						"Content-Type": "application/json",
+						Authorization: `${apiKey}:${signature}`,
+						"X-ROCK-TIMESTAMP": timestamp,
+					};
+
+					const firstname = this.getNodeParameter('firstname', 0) as string; 
+					const lastname = this.getNodeParameter('lastname', 0) as string; 
+					const domainOrCompany = this.getNodeParameter('domain', 0) as string; 
+					const bodyParameters = JSON.stringify({ firstname, lastname, domainOrCompany });
+
+					const outputData = await processApiCallSingle(URL_email_search, headers, bodyParameters);
+        			return [outputData];
+				}
+				else{
+					const timestamp = new Date().toISOString();
+					const signature = generateSignature(URL_domain_search, "POST", apiSecret, timestamp);
+					const headers = {
+						"Content-Type": "application/json",
+						Authorization: `${apiKey}:${signature}`,
+						"X-ROCK-TIMESTAMP": timestamp,
+					};
+					const domainOrCompany = this.getNodeParameter('domain', 0) as string;
+					const bodyParameters = JSON.stringify({ domainOrCompany });
+
+					const outputData = await processApiCallSingle(URL_domain_search, headers, bodyParameters);
+					return [outputData];
+				}
+			}else{
+				const taskBulk = this.getNodeParameter('taskBulk', 0);
+
+				if (!credentials.userId) {
+					throw new NodeOperationError(this.getNode(), 'Credentials are missing: userId for bulk search.');
+				}
+				const user = credentials.userId as string;
+				
+				let name = this.getNodeParameter('name', 0);
+				if (!name) name = 'Test';
+				
+				const renameColumns = this.getNodeParameter('renameColumns', 0) as {
+					firstname?: string;
+					lastname?: string;
+					domain?: string;
+					email?: string;
+				};
+
+				const URL_bulk = "https://app.icypeas.com/api/bulk-search";
+			
 				const timestamp = new Date().toISOString();
-				const signature = generateSignature(URL_email_verif, "POST", apiSecret, timestamp);
+				const signature = generateSignature(URL_bulk, "POST", apiSecret, timestamp);
+
 				const headers = {
 					"Content-Type": "application/json",
 					Authorization: `${apiKey}:${signature}`,
 					"X-ROCK-TIMESTAMP": timestamp,
 				};
 
-				const email = this.getNodeParameter('email', 0) as string; // Get the email value from the node properties
-				const bodyParameters = JSON.stringify({ email });
+				if ( taskBulk === 'email-search') {
+					const inputData = this.getInputData(0); //O : index of the first input
+					const data : any[][] = [];
+					for (let i = 0; i < inputData.length; i++) {
+						const item = inputData[i];
+						const firstName = renameColumns.firstname ? item.json[renameColumns.firstname] : item.json.firstname || '';
+						const lastName = renameColumns.lastname ? item.json[renameColumns.lastname] : item.json.lastname || '';
+						const company = renameColumns.domain ? item.json[renameColumns.domain] : item.json.company || '';
+						data.push([firstName, lastName, company]);
+					}
+					const bodyParameters = JSON.stringify({ user, name, taskBulk, data });
+	
+					const response = await fetch(URL_bulk, {
+						method: "POST",
+						headers: headers,
+						body: bodyParameters,
+					});
+	
+					const responseData: any = await response.json();
+					if (response.status === 200 && responseData.success) {
+						// If the request was successful (success = true) return results in the output data array
+						const status = responseData.status;
+						const fileId = responseData.file;
+						const outputData: INodeExecutionData[] = [
+							{
+								json: {
+									message: 'Bulk email search successful!',
+									status: status,
+									fileId: fileId,
+								},
+							},
+						];
+						return [outputData];
+	
+					} else if (response.status === 200 && responseData.validationErrors) {
+						const errorMessage = responseData.validationErrors.map((error: any) => error.message).join(', ');
+						//throw new NodeOperationError(this.getNode(), errorMessage);
+						throw new Error(errorMessage);
+					} else if (response.status === 401) {
+						throw new NodeOperationError(this.getNode(), 'Unauthorized access.');
+					} else {
+						throw new NodeOperationError(this.getNode(), 'An unknown error occurred while processing the request.');
+					}
+				}else if ( taskBulk === 'email-verification' ) {
+					const inputData = this.getInputData(0); //O : index of the first input
+					const data : any[][] = [];
+					for (let i = 0; i < inputData.length; i++) {
+						const item = inputData[i];
+						const email = renameColumns.email ? item.json[renameColumns.email] : item.json.email || '';
+						data.push([email]);
+					}
+					const bodyParameters = JSON.stringify({ user, name, taskBulk, data });
+	
+					const response = await fetch(URL_bulk, {
+						method: "POST",
+						headers: headers,
+						body: bodyParameters,
+					});
+	
+					const responseData: any = await response.json();
+					if (response.status === 200 && responseData.success) {
+						const status = responseData.status;
+						const fileId = responseData.file;
+						const outputData: INodeExecutionData[] = [
+							{
+								json: {
+									message: 'Bulk email verification successful!',
+									status: status,
+									fileId: fileId,
+								},
+							},
+						];
+						return [outputData];
+	
+					} else if (response.status === 200 && responseData.validationErrors) {
+						const errorMessage = responseData.validationErrors.map((error: any) => error.message).join(', ');
+						//throw new NodeOperationError(this.getNode(), errorMessage);
+						throw new Error(errorMessage);
+					} else if (response.status === 401) {
+						throw new NodeOperationError(this.getNode(), 'Unauthorized access.');
+					} else {
+						throw new NodeOperationError(this.getNode(), 'An unknown error occurred while processing the request.');
+					}
+				}
+				else{
+					const inputData = this.getInputData(0); //O : index of the first input
+					const data : any[][] = [];
+					for (let i = 0; i < inputData.length; i++) {
+						const item = inputData[i];
+						const company = renameColumns.domain ? item.json[renameColumns.domain] : item.json.company || '';
+						data.push([company]);
+					}
+					const bodyParameters = JSON.stringify({ user, name, taskBulk, data });
+	
+					const response = await fetch(URL_bulk, {
+						method: "POST",
+						headers: headers,
+						body: bodyParameters,
+					});
+	
+					const responseData: any = await response.json();
+	
+					if (response.status === 200 && responseData.success) {
+						const status = responseData.status;
+						const fileId = responseData.file;
+						const outputData: INodeExecutionData[] = [
+							{
+								json: {
+									message: 'Bulk domain search successful!',
+									status: status,
+									fileId: fileId,
+								},
+							},
+						];
+						return [outputData];
+	
+					} else if (response.status === 200 && responseData.validationErrors) {
+						const errorMessage = responseData.validationErrors.map((error: any) => error.message).join(', ');
+						//throw new NodeOperationError(this.getNode(), errorMessage);
+						throw new Error(errorMessage);
+					} else if (response.status === 401) {
+						throw new NodeOperationError(this.getNode(), 'Unauthorized access.');
+					} else {
+						throw new NodeOperationError(this.getNode(), 'An unknown error occurred while processing the request.');
+					}
+				}
 
-				const outputData = await processApiCallSingle(URL_email_verif, headers, bodyParameters);
-        		return [outputData];
 
-			}else if ( taskSingle === 'email-search') {
-				const timestamp = new Date().toISOString();
-				const signature = generateSignature(URL_email_search, "POST", apiSecret, timestamp);
-				const headers = {
-					"Content-Type": "application/json",
-					Authorization: `${apiKey}:${signature}`,
-					"X-ROCK-TIMESTAMP": timestamp,
-				};
-
-				const firstname = this.getNodeParameter('firstname', 0) as string; 
-				const lastname = this.getNodeParameter('lastname', 0) as string; 
-				const domainOrCompany = this.getNodeParameter('domain', 0) as string; 
-				const bodyParameters = JSON.stringify({ firstname, lastname, domainOrCompany });
-
-				const outputData = await processApiCallSingle(URL_email_search, headers, bodyParameters);
-        		return [outputData];
+				throw new NodeOperationError(this.getNode(), 'Bulk search is not yet implemented.');
 			}
-			else{
-				const timestamp = new Date().toISOString();
-				const signature = generateSignature(URL_domain_search, "POST", apiSecret, timestamp);
-				const headers = {
-					"Content-Type": "application/json",
-					Authorization: `${apiKey}:${signature}`,
-					"X-ROCK-TIMESTAMP": timestamp,
-				};
-				const domainOrCompany = this.getNodeParameter('domain', 0) as string;
-				const bodyParameters = JSON.stringify({ domainOrCompany });
-				console.log(bodyParameters);
 
-				const outputData = await processApiCallSingle(URL_domain_search, headers, bodyParameters);
-				return [outputData];
-			}
+
+
 		} catch (error) {
 			if (error instanceof Error && error.message === 'Unauthorized access.') {
 				throw new NodeOperationError(this.getNode(), 'Unauthorized access.');
 			} else if (error instanceof NodeOperationError && error.message === 'Credentials are missing.') {
 				throw new NodeOperationError(this.getNode(), 'Credentials are missing.');
+			} else if (error instanceof NodeOperationError && error.message === 'Credentials are missing: userId for bulk search.') {
+				throw new NodeOperationError(this.getNode(), 'Credentials are missing: userId for bulk search.');
 			}throw new NodeOperationError(this.getNode(), 'An unknown error occurred while processing the request.');
 		}
 	}
